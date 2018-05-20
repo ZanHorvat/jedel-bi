23878cf942a8937369f5a6c1a4473be26e98884b
HORVAT <zh5129@student.uni-lj.si>
2018-05-20 08:22:08 +0200

    Finished dnevnik

--- script/custom.js
+++ script/custom.js
@@ -3,35 +3,64 @@ var app = new Vue({
     data: {
         /*Hardcoded, but want to read from json*/
         meals: [{
-            location: 'FRI',
-            datum: '1.1.2016'
+            location: '(ročni vnos)',
+            datum: '20.5.2018',
+            obrok: `pizza`
+        }, {
+            location: '(ročni vnos)',
+            datum: '19.5.2018',
+            obrok: `hamburger`
+        }, {
+            location: '(ročni vnos)',
+            datum: '19.5.2018',
+            obrok: `pečen krompir, piščanji file`
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `jajčni rezanci v omaki s tartufato,
+            zelenjavna kremna juha,
+            sestavljena solata`
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `paradižnikova enolončnica z mesnimi cmočki in zelenjavo,
+            sestavljena solata,
+            sadje, dnevna sladica
+            `
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `solatni krožnik s tunininem filejem,
+            dnevna juha,
+            sestavljena solata            
+            `
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `puranji zrezek v omaki s sirom, testenine ali bulgur,
+            zelenjavna mineštra,
+            sestavljena solata
+            `
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `svinjski kotlet na žaru, krompirjevi krhlji in ajvar,
+            zelenjavna kremna juha,
+            sestavljena solata
+            `
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `pečeno piščančje bedro, stročji fižol z gobicami in krompirjevi krhlji,
+            zelenjavna kremna juha,
+            sestavljena solata`
         }, {
             location: 'FKKT',
-            datum: '1.2.2016'
-        }, {
-            location: 'FKKT',
-            datum: '1.2.2016'
-        }, {
-            location: 'FKKT',
-            datum: '1.2.2016'
+            datum: '1.2.2016',
+            obrok: `pašta fižol,
+            sestavljena solata,
+            sadje, dnevna sladica            
+            `
         }
 
         ],
