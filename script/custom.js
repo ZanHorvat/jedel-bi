8ad67a1f0f0e29c328d5ae475c47a37c2fe4fd60
HORVAT <zh5129@student.uni-lj.si>
2018-05-20 02:43:49 +0200

    Added vue js in Dnevnik

---
+++ script/custom.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#dnevnik-app',
+    data: {
+        /*Hardcoded, but want to read from json*/
+        meals: [{
+            location: 'FRI',
+            datum: '1.1.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }, {
+            location: 'FKKT',
+            datum: '1.2.2016'
+        }
+
+        ],
+    }
+})
